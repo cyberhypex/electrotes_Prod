2edf3cee9c5c328f0ca82fc8329e3ac46740565b
Anshuman <chaoanxuman999@gmail.com>
2024-06-09 20:37:34 +0530

    Made changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'Screens/homeScreen.dart';
+import 'Screens/splashScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,12 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Electrotes',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white54),
         useMaterial3: true,
       ),
-      home: MyHomePage(),
+      home: SplashScreen(),
     );
   }
 }
