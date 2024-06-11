5fce6bc50762e6785d03390a280b1e6c94fb0fa9
Anshuman <chaoanxuman999@gmail.com>
2024-06-11 11:59:25 +0530

    Made Home Screen UI and created every semester pages. TODO:Add button

--- lib/Screens/homeSems.dart
+++ lib/Screens/homeSems.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeSems extends StatefulWidget {
   const HomeSems({super.key});
@@ -10,6 +13,164 @@ class HomeSems extends StatefulWidget {
 class _HomeSemsState extends State<HomeSems> {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.white, child: Text('Hii'));
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 10,
+        title: Text(
+          'Semester?',
+          style: GoogleFonts.aldrich(
+            textStyle: const TextStyle(
+                color: Colors.black87,
+                fontSize: 25,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+      body: Container(
+          height: double.infinity,
+          width: double.maxFinite,
+          child: SafeArea(
+            child: ListView(
+              children: [
+                Container(
+                    height: MediaQuery.sizeOf(context).height * 0.1,
+                    //  color: Colors.black45,
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(21, 30, 39, 200),
+                      shape: BoxShape.rectangle,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "3rd semester",
+                        style: GoogleFonts.aldrich(
+                          textStyle: const TextStyle(
+                              color: Colors.black87,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w300),
+                        ),
+                      ),
+                    )),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    height: MediaQuery.sizeOf(context).height * 0.1,
+                    //  color: Colors.black45,
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(21, 30, 39, 200),
+                      shape: BoxShape.rectangle,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "4th semester",
+                        style: GoogleFonts.aldrich(
+                          textStyle: const TextStyle(
+                              color: Colors.black87,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w300),
+                        ),
+                      ),
+                    )),
+                const SizedBox(
+                  height: 25,
+                ),
+                Container(
+                    height: MediaQuery.sizeOf(context).height * 0.1,
+                    //  color: Colors.black45,
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(21, 30, 39, 200),
+                      shape: BoxShape.rectangle,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "5th semester",
+                        style: GoogleFonts.aldrich(
+                          textStyle: const TextStyle(
+                              color: Colors.black87,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w300),
+                        ),
+                      ),
+                    )),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    height: MediaQuery.sizeOf(context).height * 0.1,
+                    //  color: Colors.black45,
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(21, 30, 39, 200),
+                      shape: BoxShape.rectangle,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "6th semester",
+                        style: GoogleFonts.aldrich(
+                          textStyle: const TextStyle(
+                              color: Colors.black87,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w300),
+                        ),
+                      ),
+                    )),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    height: MediaQuery.sizeOf(context).height * 0.1,
+                    //  color: Colors.black45,
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(21, 30, 39, 200),
+                      shape: BoxShape.rectangle,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "7th semester",
+                        style: GoogleFonts.aldrich(
+                          textStyle: const TextStyle(
+                              color: Colors.black87,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w300),
+                        ),
+                      ),
+                    )),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    height: MediaQuery.sizeOf(context).height * 0.1,
+                    //  color: Colors.black45,
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(21, 30, 39, 200),
+                      shape: BoxShape.rectangle,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "8th semester",
+                        style: GoogleFonts.aldrich(
+                          textStyle: const TextStyle(
+                              color: Colors.black87,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w300),
+                        ),
+                      ),
+                    )),
+                const SizedBox(
+                  height: 20,
+                ),
+                Center(
+                  child: Text("Developed By Anshuman",
+                      style: GoogleFonts.acme(
+                          textStyle: const TextStyle(
+                              color: Colors.black87,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold))),
+                )
+              ],
+            ),
+          )),
+    );
   }
 }
