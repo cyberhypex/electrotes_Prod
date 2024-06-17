83542b0172f5703c8a69d7d1e9b566a8d4a210d9
Anshuman <chaoanxuman999@gmail.com>
2024-06-17 17:57:14 +0530

    Subject added for 3rd sem. TODO accomplished

--- lib/3rd Sem/thirdSem.dart
+++ lib/3rd Sem/thirdSem.dart
@@ -14,38 +14,158 @@ class _thirdSemesterState extends State<thirdSemester> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-
-        title: Text(
-          'Subject?',
-          style: GoogleFonts.aldrich(
-            textStyle: const TextStyle(
-                color: Colors.black87,
-                fontSize: 25,
-                fontWeight: FontWeight.bold),
+        appBar: AppBar(
+          title: Text(
+            'Subject?',
+            style: GoogleFonts.aldrich(
+              textStyle: const TextStyle(
+                  color: Colors.black87,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold),
+            ),
           ),
-        ),
-        leading: IconButton(
-            iconSize: MediaQuery.sizeOf(context).width * 0.1,
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(Icons.arrow_back_sharp)),
-        centerTitle: true,
-        actions: [
-          IconButton(
+          leading: IconButton(
               iconSize: MediaQuery.sizeOf(context).width * 0.1,
               onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => HomeSems()));
+                Navigator.pop(context);
               },
-              icon: const Icon(Icons.home_outlined))
-        ],
-      ),
-    //todo:Add listview
-    //todo:Add ListView & Make Subjects page
-    
-    
-    );
+              icon: const Icon(Icons.arrow_back_sharp)),
+          centerTitle: true,
+          actions: [
+            IconButton(
+                iconSize: MediaQuery.sizeOf(context).width * 0.1,
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomeSems()));
+                },
+                icon: const Icon(Icons.home_outlined))
+          ],
+        ),
+        //todo:Add listview
+        //todo:Add ListView & Make Subjects page
+
+        body: Container(
+          height: double.infinity,
+          width: double.maxFinite,
+          child: SafeArea(
+            child: ListView(children: [
+              Container(
+                  height: MediaQuery.sizeOf(context).height * 0.1,
+                  //  color: Colors.black45,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(21, 30, 39, 200),
+                    shape: BoxShape.rectangle,
+                  ),
+                  child: Center(
+                      child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Signals & Systems",
+                      style: GoogleFonts.acme(
+                        textStyle: const TextStyle(
+                            color: Colors.black87,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                  ))),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                  height: MediaQuery.sizeOf(context).height * 0.1,
+                  //  color: Colors.black45,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(21, 30, 39, 200),
+                    shape: BoxShape.rectangle,
+                  ),
+                  child: Center(
+                      child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Semiconductor Devices",
+                      style: GoogleFonts.acme(
+                        textStyle: const TextStyle(
+                            color: Colors.black87,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                  ))),
+              const SizedBox(
+                height: 25,
+              ),
+              Container(
+                  height: MediaQuery.sizeOf(context).height * 0.1,
+                  //  color: Colors.black45,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(21, 30, 39, 200),
+                    shape: BoxShape.rectangle,
+                  ),
+                  child: Center(
+                      child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Network theory",
+                      style: GoogleFonts.acme(
+                        textStyle: const TextStyle(
+                            color: Colors.black87,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                  ))),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                  height: MediaQuery.sizeOf(context).height * 0.1,
+                  //  color: Colors.black45,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(21, 30, 39, 200),
+                    shape: BoxShape.rectangle,
+                  ),
+                  child: Center(
+                      child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Digital Circuits",
+                      style: GoogleFonts.acme(
+                        textStyle: const TextStyle(
+                            color: Colors.black87,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                  ))),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                  height: MediaQuery.sizeOf(context).height * 0.1,
+                  //  color: Colors.black45,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(21, 30, 39, 200),
+                    shape: BoxShape.rectangle,
+                  ),
+                  child: Center(
+                      child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Maths-3",
+                      style: GoogleFonts.acme(
+                        textStyle: const TextStyle(
+                            color: Colors.black87,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                  ))),
+              const SizedBox(
+                height: 20,
+              ),
+            ]),
+          ),
+        ));
   }
 }
