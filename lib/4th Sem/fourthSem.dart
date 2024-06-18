f9ae434dbd77057ce00b5b9f304cca027a1b05a8
Anshuman <chaoanxuman999@gmail.com>
2024-06-18 20:52:37 +0530

    Subjects added to 4th semester

--- lib/4th Sem/fourthSem.dart
+++ lib/4th Sem/fourthSem.dart
@@ -14,34 +14,155 @@ class _fourthSemesterState extends State<fourthSemester> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-
-        title: Text(
-          'Subject?',
-          style: GoogleFonts.aldrich(
-            textStyle: const TextStyle(
-                color: Colors.black87,
-                fontSize: 25,
-                fontWeight: FontWeight.bold),
+        appBar: AppBar(
+          title: Text(
+            'Subject?',
+            style: GoogleFonts.aldrich(
+              textStyle: const TextStyle(
+                  color: Colors.black87,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold),
+            ),
           ),
-        ),
-        leading: IconButton(
-            iconSize: MediaQuery.sizeOf(context).width * 0.1,
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(Icons.arrow_back_sharp)),
-        centerTitle: true,
-        actions: [
-          IconButton(
+          leading: IconButton(
               iconSize: MediaQuery.sizeOf(context).width * 0.1,
               onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => HomeSems()));
+                Navigator.pop(context);
               },
-              icon: const Icon(Icons.home_outlined))
-        ],
-      ),
-    );
+              icon: const Icon(Icons.arrow_back_sharp)),
+          centerTitle: true,
+          actions: [
+            IconButton(
+                iconSize: MediaQuery.sizeOf(context).width * 0.1,
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomeSems()));
+                },
+                icon: const Icon(Icons.home_outlined))
+          ],
+        ),
+        body: Container(
+          height: double.infinity,
+          width: double.maxFinite,
+          child: SafeArea(
+            child: ListView(children: [
+              Container(
+                  height: MediaQuery.sizeOf(context).height * 0.1,
+                  //  color: Colors.black45,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(21, 30, 39, 200),
+                    shape: BoxShape.rectangle,
+                  ),
+                  child: Center(
+                      child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "LARP",
+                      style: GoogleFonts.acme(
+                        textStyle: const TextStyle(
+                            color: Colors.black87,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                  ))),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                  height: MediaQuery.sizeOf(context).height * 0.1,
+                  //  color: Colors.black45,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(21, 30, 39, 200),
+                    shape: BoxShape.rectangle,
+                  ),
+                  child: Center(
+                      child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Analog Circuits",
+                      style: GoogleFonts.acme(
+                        textStyle: const TextStyle(
+                            color: Colors.black87,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                  ))),
+              const SizedBox(
+                height: 25,
+              ),
+              Container(
+                  height: MediaQuery.sizeOf(context).height * 0.1,
+                  //  color: Colors.black45,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(21, 30, 39, 200),
+                    shape: BoxShape.rectangle,
+                  ),
+                  child: Center(
+                      child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Analog Communication",
+                      style: GoogleFonts.acme(
+                        textStyle: const TextStyle(
+                            color: Colors.black87,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                  ))),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                  height: MediaQuery.sizeOf(context).height * 0.1,
+                  //  color: Colors.black45,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(21, 30, 39, 200),
+                    shape: BoxShape.rectangle,
+                  ),
+                  child: Center(
+                      child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Instrumentation",
+                      style: GoogleFonts.acme(
+                        textStyle: const TextStyle(
+                            color: Colors.black87,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                  ))),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                  height: MediaQuery.sizeOf(context).height * 0.1,
+                  //  color: Colors.black45,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(21, 30, 39, 200),
+                    shape: BoxShape.rectangle,
+                  ),
+                  child: Center(
+                      child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "EEM",
+                      style: GoogleFonts.acme(
+                        textStyle: const TextStyle(
+                            color: Colors.black87,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                  ))),
+              const SizedBox(
+                height: 20,
+              ),
+            ]),
+          ),
+        ));
   }
 }
