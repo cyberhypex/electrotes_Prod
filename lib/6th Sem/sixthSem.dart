e739dc49c6ee5a1629f3b7c84774f3f749ff8dbe
Anshuman <chaoanxuman999@gmail.com>
2024-06-21 22:40:04 +0530

    unavailability of 6th sem notes fixed

--- lib/6th Sem/sixthSem.dart
+++ lib/6th Sem/sixthSem.dart
@@ -14,35 +14,49 @@ class _sixthSemesterState extends State<sixthSemester> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Subject?',
-          style: GoogleFonts.aldrich(
-            textStyle: const TextStyle(
-                color: Colors.black87,
-                fontSize: 25,
-                fontWeight: FontWeight.bold),
+        appBar: AppBar(
+          title: Text(
+            'Subject?',
+            style: GoogleFonts.aldrich(
+              textStyle: const TextStyle(
+                  color: Colors.black87,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold),
+            ),
           ),
-        ),
-        leading: IconButton(
-            iconSize: MediaQuery.sizeOf(context).width * 0.1,
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(Icons.arrow_back_sharp)),
-        centerTitle: true,
-        actions: [
-          IconButton(
+          leading: IconButton(
               iconSize: MediaQuery.sizeOf(context).width * 0.1,
               onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => HomeSems()));
+                Navigator.pop(context);
               },
-              icon: const Icon(Icons.home_outlined))
-        ],
-      ),
-      //todo:Add listview
-      //todo:Add ListView & Make Subjects page
-    );
+              icon: const Icon(Icons.arrow_back_sharp)),
+          centerTitle: true,
+          actions: [
+            IconButton(
+                iconSize: MediaQuery.sizeOf(context).width * 0.1,
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomeSems()));
+                },
+                icon: const Icon(Icons.home_outlined))
+          ],
+        ),
+        body: Container(
+            height: double.infinity,
+            width: double.infinity,
+            child: Center(
+              child: Text(
+                "Coming Soon!!",
+                style: GoogleFonts.acme(
+                  textStyle: const TextStyle(
+                      color: Colors.black87,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w300),
+                ),
+              ),
+            ))
+        //todo:Add listview
+        //todo:Add ListView & Make Subjects page
+        );
   }
 }
