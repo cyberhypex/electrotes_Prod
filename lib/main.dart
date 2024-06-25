0eca04c1c0b1e8792e50517b228c8b1a04f63e12
Anshuman <chaoanxuman999@gmail.com>
2024-06-26 00:48:24 +0530

    Firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:electrotes/Screens/homeSems.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+// ...
 
 //import 'Screens/splashScreen.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
 }
 
 class MyApp extends StatelessWidget {
